354fe5facb54762916d33dc3b277caef933e6e83
Josh Newsom <newsommeister@gmail.com>
2017-01-06 22:21:38 -0700

    Add gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,33 @@ var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
   concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  console.log('Gulp!');
-});
+gulp.task('default', [
+  'lint-client',
+  'concat',
+  'watch-js'
+]);
 
-gulp.task('lint-client', function () {
+gulp.task('lint-client', function() {
   return gulp.src(__dirname + '/client/**/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('concat', function() {
+  return gulp.src([
+      __dirname + '/client/**/*.js',
+      '!' + __dirname + '/client/dist/**'
+    ])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest(__dirname + '/client/dist/'));
+});
+
+gulp.task('watch-js', function() {
+  return gulp.watch([
+    __dirname + '/client/**/*.js',
+    '!' + __dirname + '/client/dist/**'
+  ], [
+    'lint-client',
+    'concat'
+  ]);
 });
