237b6cc89f285442086923aa4d4873d0a534083a
Josh Newsom <newsommeister@gmail.com>
2017-01-17 20:33:32 -0700

    Update gulpfile with production build

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,11 @@ var gulp = require('gulp'),
   sass = require('gulp-sass'),
   sassLint = require('gulp-sass-lint'),
   postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer');
+  autoprefixer = require('autoprefixer'),
+  minify = require('gulp-minify'),
+  cleanCSS = require('gulp-clean-css'),
+  flatten = require('gulp-flatten'),
+  fs = require('fs');
 
 gulp.task('default', [
   'lint-client',
@@ -20,19 +24,11 @@ gulp.task('default', [
 gulp.task('lint-client', function() {
   return gulp.src([
       __dirname + '/client/**/*.js',
-      '!' + __dirname + '/client/public/**'
+      '!' + __dirname + '/client/public/**',
+      '!**/dist/**'
     ])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('concat', function() {
-  return gulp.src([
-      __dirname + '/client/**/*.js',
-      '!' + __dirname + '/client/dist/**'
-    ])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest(__dirname + '/client/dist/'));
 });
 
 gulp.task('watch-js', function() {
@@ -48,7 +44,8 @@ gulp.task('inject', ['autoprefixer'], function() {
   var target = gulp.src(__dirname + '/client/index.html'),
     sources = gulp.src([
       __dirname + '/client/**/*.js',
-      __dirname + '/client/**/*.css'
+      __dirname + '/client/**/*.css',
+      '!**/dist/**'
     ], {
       read: false
     });
@@ -63,7 +60,8 @@ gulp.task('sass', function() {
       __dirname + '/**/*.scss',
       '!' + __dirname + '/node_modules/**',
       '!**/variables.scss',
-      '!**/mixins.scss'
+      '!**/mixins.scss',
+      '!**/dist/**'
     ])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('.'));
@@ -83,14 +81,65 @@ gulp.task('sass-lint', function() {
   return gulp.src([
       __dirname + '/**/*.scss',
       '!' + __dirname + '/node_modules/**',
-      '!**/mixins.scss'
+      '!**/mixins.scss',
+      '!**/dist/**'
     ])
     .pipe(sassLint())
     .pipe(sassLint.format());
 });
 
 gulp.task('autoprefixer', ['sass'], function() {
-  return gulp.src(__dirname + '/**/*.css')
+  return gulp.src([
+      __dirname + '/**/*.css',
+      '!**/dist/**'
+    ])
     .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest('.'));
+});
+
+
+// Production Tasks
+
+gulp.task('production', [
+  'css-production',
+  'js-production',
+  'inject-production'
+]);
+
+gulp.task('css-production', function() {
+  return gulp.src([
+      __dirname + '/**/*.scss',
+      '!' + __dirname + '/node_modules/**',
+      '!**/variables.scss',
+      '!**/mixins.scss'
+    ])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(flatten())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(concat('all.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(__dirname + '/client/dist'));
+});
+
+gulp.task('js-production', function() {
+  return gulp.src([
+      __dirname + '/client/**/*.js',
+      '!' + __dirname + '/node_modules/**'
+    ])
+    .pipe(concat('all.js'))
+    .pipe(minify())
+    .pipe(gulp.dest(__dirname + '/client/dist'));
+});
+
+gulp.task('inject-production', ['js-production', 'css-production'], function() {
+  fs.unlink(__dirname + '/client/dist/all.js');
+  var target = gulp.src(__dirname + '/client/index.html'),
+    sources = gulp.src([
+      __dirname + '/dist/all-min.js',
+      __dirname + '/dist/all.css'
+    ], {
+      read: false
+    });
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest(__dirname + '/client'));
 });
